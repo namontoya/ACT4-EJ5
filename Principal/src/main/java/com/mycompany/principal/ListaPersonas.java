1ae76599084616521b076f9f98c811685da77a52
namontoya <161357317+namontoya@users.noreply.github.com>
2024-08-20 18:48:38 -0500

    Add files via upload

---
+++ Principal/src/main/java/com/mycompany/principal/ListaPersonas.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.principal;
+import java.util.*;
+
+/**
+ *
+ * @author ASUS
+ */
+public class ListaPersonas {
+    Vector listaPersonas; // Atributo que identifica un vector de personas
+    /**
+    * Constructor de la clase ListaPersonas
+    */
+    public ListaPersonas() {
+        listaPersonas = new Vector(); // Crea el vector de personas
+    }
+    /**
+    * Método que permite agregar una persona al vector de personas
+    * @param p Parámetro que define la persona a agregar al vector de
+    * personas
+    */
+    
+    public void añadirPersona(Persona p) {
+        listaPersonas.add(p);
+    }
+    /**
+    * Método que permite eliminar una persona del vector de personas
+    * @param i Parámetro que define la posición a eliminar en el vector
+    * de personas
+    */
+    public void eliminarPersona(int i) {
+        listaPersonas.removeElementAt(i);
+    }
+    /**
+    * Método que permite eliminar todos los elementos del vector de
+    * personas
+    */
+    public void borrarLista() {
+        listaPersonas.removeAllElements();
+    }
+}
---
+++ Principal/src/main/java/com/mycompany/principal/Principal.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.principal;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Principal {
+
+    public static void main(String[] args) {
+        VentanaPrincipal miVentanaPrincipal; /* Define la ventana
+        principal */
+        miVentanaPrincipal= new VentanaPrincipal(); /* Crea la ventana
+        principal */
+        miVentanaPrincipal.setVisible(true); /* Establece la ventana
+        como visible */
+    }
+}
